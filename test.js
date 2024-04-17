ad89fb10b203ed5304eb419c89b875e91b1b1fc3
SenorCompost <87546829+SenorCompost@users.noreply.github.com>
2024-04-17 13:40:28 -0500

    Update test.js

--- test.js
+++ test.js
@@ -1,5 +1,7 @@
-function moo() {
+(function() {
+// >>>> start of private namespace
+'use strict';
   document.addEventListener("DOMContentLoaded", (event) => {
     alert("DOM fully loaded and parsed");
   });
-}
+})
