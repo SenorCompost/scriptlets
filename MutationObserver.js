c5023d1a900050c80b16166b238e3bfd4f966a91
SenorCompost <87546829+SenorCompost@users.noreply.github.com>
2024-06-21 15:04:23 -0500

    Update MutationObserver.js

--- MutationObserver.js
+++ MutationObserver.js
@@ -1,11 +1,13 @@
-MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
-
-var observer = new MutationObserver(function(mutations, observer) {
-    console.log(mutations, observer);
-});
-
-observer.observe(document.querySelector("div.pictures-wrapper"), {
-  childList: true,
-  subtree: true,
-  attributes: true,
-});
+function myScript(){
+    MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
+    
+    var observer = new MutationObserver(function(mutations, observer) {
+        console.log(mutations, observer);
+    });
+    
+    observer.observe(document.querySelector("div.pictures-wrapper"), {
+      childList: true,
+      subtree: true,
+      attributes: true,
+    });
+}
