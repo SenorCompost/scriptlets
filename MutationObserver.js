af2a58136aa730852ca0e4933b91ff8e36cf6263
SenorCompost <87546829+SenorCompost@users.noreply.github.com>
2024-06-22 14:51:48 -0500

    Update MutationObserver.js

--- MutationObserver.js
+++ MutationObserver.js
@@ -1,13 +1,22 @@
-function myScript(){
-    MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
-    
-    var observer = new MutationObserver(function(mutations, observer) {
-        console.log(mutations, observer);
-    });
-    
-    observer.observe(document.querySelector("div.pictures-wrapper"), {
-      childList: true,
-      subtree: true,
-      attributes: true,
-    });
+async function removeProtectedGarlleries(){
+	MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
+
+	var observer = new MutationObserver(function(mutations, observer) {
+		if(mutations[0].target.className == 'media-lazy-load' || mutations[0].target.className == 'pictures-wrapper') {
+			for(var ele of document.querySelectorAll("img[src*='gallery-locked']")) {
+				ele.parentElement.parentElement.parentElement.remove();
+				await sleep(500);
+			}
+		}
+	});
+
+	observer.observe(document, {
+	  childList: true,
+	  subtree: true,
+	  attributes: true,
+	});
+}
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
 }
