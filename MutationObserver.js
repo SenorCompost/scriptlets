a7bda2c01ab904db212c94365a312c559b3b5ba4
SenorCompost <87546829+SenorCompost@users.noreply.github.com>
2024-06-22 21:21:48 -0500

    Update MutationObserver.js

--- MutationObserver.js
+++ MutationObserver.js
@@ -4,7 +4,8 @@ function removeProtectedGarlleries(){
 	var observer = new MutationObserver(function(mutations, observer) {
 		if(mutations[0].target.className == 'media-lazy-load' || mutations[0].target.className == 'pictures-wrapper') {
 			for(let ele of document.querySelectorAll("img[src*='/media/gallery-locked']")) {
-				ele.parentElement.parentElement.parentElement.style.display = "none"; ele.src = '';
+				ele.parentElement.parentElement.parentElement.style.display = "none";
+				ele.src = '';
 			}
 		}
 	});
