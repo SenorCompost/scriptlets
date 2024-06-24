e8b4e90030e1ad2475bd141a0e3771a27bb49ea5
SenorCompost <87546829+SenorCompost@users.noreply.github.com>
2024-06-24 10:18:23 -0500

    Update loadJavascript.js

--- loadJavascript.js
+++ loadJavascript.js
@@ -1,9 +1,10 @@
 function loadJS(jsURL){
 	var script = document.createElement('script');
 	script.setAttribute("src", jsURL);
+	script.setAttribute("id", "myScript");
 	script.setAttribute("type", "text/javascript");
 	script.setAttribute("async", true);
-
+	
 	document.body.appendChild(script);
 
 	script.addEventListener("load", () => {
