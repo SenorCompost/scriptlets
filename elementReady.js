25f879449bda1ef539a508d9c5f905c88ea84b5c
SenorCompost <87546829+SenorCompost@users.noreply.github.com>
2024-09-28 18:18:27 -0500

    Create elementReady.js

---
+++ elementReady.js
@@ -0,0 +1,22 @@
+function elementReady(selector) {
+  return new Promise((resolve, reject) => {
+    let el = document.querySelector(selector);
+
+    if(el){
+      return resolve(el);
+    }
+
+  var observer = new MutationObserver((mutationRecords, observer) => {
+    let elem = document.querySelector(selector);
+      if(elem) {
+        resolve(elem);
+        observer.disconnect();
+      }
+    });
+
+  observer.observe(document.documentElement, {
+        childList: true,
+        subtree: true
+      });
+  });
+}
